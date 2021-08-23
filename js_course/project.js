338cb20ad9dc3a918f95d92dfbd2d09811a119cb
Iaroslav <kholiavkoi@gmail.com>
2021-08-23 08:19:08 +0300

    new lines

--- js_course/project.js
+++ js_course/project.js
@@ -1,40 +1,44 @@
-var humidity = document.getElementById('current-humidity'),
-    pressure = document.getElementById('current-pressure'),
-    temperature = document.getElementById('current-temperature'),
-    windSpeed = document.getElementById('current-wind-speed'),
-    getWeatherButton = document.getElementById('get-weather'),
-    weatherSummary = document.getElementById('weather-summary');
+var humidity = document.getElementById("current-humidity"),
+    pressure = document.getElementById("current-pressure"),
+    temperature = document.getElementById("current-temperature"),
+    windSpeed = document.getElementById("current-wind-speed"),
+    getWeatherButton = document.getElementById("get-weather"),
+    weatherSummary = document.getElementById("weather-summary");
 
-
-function getWeatherCoordinates () {
+function getWeatherCoordinates() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
+        navigator.geolocation.getCurrentPosition(function(position) {
             getWeatherData(position.coords.latitude, position.coords.longitude);
-        })
+        });
     } else {
-        return alert('Cant find your location')
+        return alert("Cant find your location");
     }
 }
 
-function getWeatherData (latitude, longitude) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + '7b0b73056c2f4aee9a7e5125e9d919b4') 
+function getWeatherData(latitude, longitude) {
+    fetch(
+            "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + "7b0b73056c2f4aee9a7e5125e9d919b4"
+        )
         .then(function(response) {
             return response.json();
         })
-        .then(function (data) {
+        .then(function(data) {
             displayData(data);
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log(error);
-        })
-
+        });
 }
 
 function displayData(data) {
-    temperature.innerHTML = 'Temperature: ' + kelvinToCelsius(data.main.temp) + 'C';
+    console.log(data.main.temp);
+    temperature.innerHTML =
+        "Temperature: " + kelvinToCelsius(data.main.temp) + "C";
+    console.log(data.main.temp);
 }
 
+function kelvinToCelsius() {
+    data.main.temp = data.main.temp - 273.15;
+}
 
-
-
-getWeatherButton.addEventListener('click', getWeatherCoordinates);
+getWeatherButton.addEventListener("click", getWeatherCoordinates);
--- js_course/script.js
+++ js_course/script.js
@@ -21,3 +21,51 @@
 // }
 
 // fn();
+
+// function BestWishes() {
+//     this.a = 4;
+//     this.b = 10;
+//     this.fn = function() {
+//         return this;
+//     }
+// }
+
+// var bestWishes = new BestWishes()
+
+// console.log(bestWishes);
+
+
+// var arr = [1, 2, 3];
+// var arr1 = arr;
+// arr1.push(4);
+// console.log(arr, arr1);
+
+// var str = "Hello"
+// var str1 = str;
+// str += ', Bob';
+
+// console.log(str, str1);
+
+
+var container = document.getElementById('container');
+container.addEventListener('click', function(event) {
+    if (event.target.className != 'remove-button') return;
+
+    var pane = event.target.closest('.pane');
+    pane.remove()
+})
+
+for (let li of tree.querySelectorAll('li')) {
+    let span = document.createElement('span');
+    li.prepend(span);
+    span.append(span.nextSibling); // поместить текстовый узел внутрь элемента <span>
+}
+
+tree.addEventListener('click', function(e) {
+    if (e.target.tagName != 'SPAN') return;
+    let childrenContainer = e.target.parentNode.querySelector('ul')
+    console.log(childrenContainer)
+    if (!childrenContainer) return;
+    childrenContainer.hidden = !childrenContainer.hidden;
+
+})
