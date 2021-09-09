1e39e3729f4f445e4a09bb3c6b4599202de19d0c
Iaroslav <kholiavkoi@gmail.com>
2021-09-09 22:33:03 +0300

    proxyy

--- proxy/script.js
+++ proxy/script.js
@@ -1,3 +1,5 @@
+//OBJECTS
+
 const person = {
     name: 'Iaroslav',
     age: 29,
@@ -6,7 +8,10 @@ const person = {
 
 const op = new Proxy(person, {
     get(target, prop) {
-        console.log(`Getting prop: ${prop}`);
+        // console.log(`Getting prop: ${prop}`);
+        if (!(prop in target)) {
+            return prop.split('_').map(p => target[p]).join(' ')
+        }
         return target[prop]
     },
     set(target, prop, value) {
@@ -15,5 +20,49 @@ const op = new Proxy(person, {
         } else {
             throw new Error(`No such ${prop} field in target`)
         }
+    },
+    has(target, prop) {
+        return ['name', 'age', 'job'].includes(prop)
+    },
+    deleteProperty(target, prop) {
+        console.log('Deleting...', prop);
+        delete target[prop]
+        return true
     }
 })
+
+
+//FUNCTIONS
+
+const log = text => `Log: ${text}`
+
+const fp = new Proxy(log, {
+    apply(target, thisArg, args) {
+        console.log(target, thisArg, args);
+
+        return target.apply(thisArg, args).toUpperCase()
+    }
+})
+
+//CLASSES
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const PersonProxy = new Proxy(Person, {
+    construct(target, args) {
+        console.log('Construct...');
+        return new Proxy(new target(...args), {
+            get(t, prop) {
+                console.log(`Getting prop: ${prop}`);
+                return t[prop]
+            }
+        })
+    }
+})
+
+const p = new PersonProxy('Maxim', 30)
