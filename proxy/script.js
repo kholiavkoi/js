e8c507256d23020130651a02b8e0c80962d484ae
iaroslav.k <yaroslav.kholiavko@triogeeks.com>
2021-09-14 09:43:58 +0300

    proxy

--- proxy/script.js
+++ proxy/script.js
@@ -1,19 +1,79 @@
-const person = {
-    name: 'Iaroslav',
-    age: 29,
-    job: 'Frontend'
+// const person = {
+// 	name: 'Iaroslav',
+// 	age: 29,
+// 	job: 'Frontend',
+// }
+
+// const op = new Proxy(person, {
+// 	get(target, prop) {
+// 		console.log(`Getting prop: ${prop}`)
+// 		return target[prop]
+// 	},
+// 	set(target, prop, value) {
+// 		if (prop in target) {
+// 			target[prop] = value
+// 		} else {
+// 			throw new Error(`No such ${prop} field in target`)
+// 		}
+// 	},
+// })
+
+//WRAPPER
+
+const withDefaultValue = (target, defaultValue = 0) => {
+	return new Proxy(target, {
+		get: (obj, prop) => (prop in obj ? obj[prop] : defaultValue),
+	})
 }
 
-const op = new Proxy(person, {
-    get(target, prop) {
-        console.log(`Getting prop: ${prop}`);
-        return target[prop]
-    },
-    set(target, prop, value) {
-        if (prop in target) {
-            target[prop] = value
-        } else {
-            throw new Error(`No such ${prop} field in target`)
-        }
-    }
+const position = withDefaultValue(
+	{
+		x: 24,
+		y: 42,
+	},
+	0
+)
+
+//HIDDEN PROPERTIES
+
+const withHiddenProps = (target, prefix = '_') => {
+	return new Proxy(target, {
+		has: (obj, prop) => prop in obj && !prop.startsWith(prefix),
+		ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
+		get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0),
+	})
+}
+
+const data = withHiddenProps({
+	name: 'Iaroslav',
+	age: 25,
+	_uid: '1233132',
 })
+
+//OPTIMIZATION
+
+const IndexedArray = new Proxy(Array, {
+	construct(target, [args]) {
+		const index = {}
+		args.forEach(item => (index[item.id] = item))
+		return new Proxy(new target(...args), {
+			get(arr, prop) {
+				switch (prop) {
+					case 'push':
+						return item => {
+							arr[prop]
+						}
+					default:
+						return arr[prop]
+				}
+			},
+		})
+	},
+})
+
+const users = new IndexedArray([
+	{id: 11, name: 'Vladilen', job: 'Fullstack', age: 25},
+	{id: 22, name: 'Elena', job: 'Student', age: 22},
+	{id: 33, name: 'Victor', job: 'Backend', age: 23},
+	{id: 44, name: 'Vasilisa', job: 'Teacher', age: 24},
+])
