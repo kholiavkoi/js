37c053fd45f68b87b7ce9e66e3c35c8e1e5abf7a
Iaroslav <kholiavkoi@gmail.com>
2021-09-20 22:55:31 +0300

    modal

--- practice/modal/index.js
+++ practice/modal/index.js
@@ -1,1 +1,24 @@
-const modal = $.modal({})
+const modal = $.modal({
+    title: 'Iaroslav Modal',
+    closable: true,
+    content: `
+    <h4>Modal is working</h4>
+    <p>Some text in modal window</p>
+    `,
+    width: '400px',
+    footerButtons: [{
+            text: 'Ok',
+            type: 'primary',
+            handler() {
+                console.log('Btn primary clicked');
+            }
+        },
+        {
+            text: 'Cancel',
+            type: 'danger',
+            handler() {
+                console.log('Btn danger clicked');
+            }
+        }
+    ]
+})
