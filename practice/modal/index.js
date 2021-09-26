c7682b48df4d5e4802d961e5be942201b94eeb7e
Iaroslav <kholiavkoi@gmail.com>
2021-09-26 22:41:54 +0300

    fix

--- practice/modal/index.js
+++ practice/modal/index.js
@@ -1,34 +1,64 @@
-const fruits = [
-    { id: 1, title: "Яблоки", price: 20, img: 'https://klike.net/uploads/posts/2019-06/1561279259_1.jpg' },
-    { id: 2, title: "Вишня", price: 40, img: 'https://foodcity.ru/storage/products/October2018/NTqwdSD2SiXVflQqOfgi.jpg' },
-    { id: 3, title: "Мандарин", price: 30, img: 'https://caoinform.moscow/wp-content/uploads/sites/38/2019/11/3_1-2-1024x881.jpg' },
-]
+const fruits = [{
+        id: 1,
+        title: 'Яблоки',
+        price: 20,
+        img: 'https://klike.net/uploads/posts/2019-06/1561279259_1.jpg',
+    },
+    {
+        id: 2,
+        title: 'Вишня',
+        price: 40,
+        img: 'https://foodcity.ru/storage/products/October2018/NTqwdSD2SiXVflQqOfgi.jpg',
+    },
+    {
+        id: 3,
+        title: 'Мандарин',
+        price: 30,
+        img: 'https://caoinform.moscow/wp-content/uploads/sites/38/2019/11/3_1-2-1024x881.jpg',
+    },
+];
 
+const toHtml = fruit => `
+    <div class="col">
+        <div class="card">
+            <img style="height: 300px;" src='${fruit.img}' class=" card-img-top ">
+            <div class="card-body ">
+                <h5 class="card-title ">${fruit.title}</h5>
+                <a href="# "class="btn btn-primary" data-btn='price' data-id='${fruit.id}'>Посмотреть цену</a>
+                <a href="# "class="btn btn-danger">Удалить</a>
+            </div>
+        </div>
+    </div>
+`;
 
-const modal = $.modal({
-    title: 'Iaroslav Modal',
+function render() {
+    const html = fruits.map(fruit => toHtml(fruit)).join('');
+    document.querySelector('#fruits').innerHTML = html;
+}
+
+render();
+
+const priceModal = $.modal({
+    title: 'Цена на товар',
     closable: true,
-    content: `
-        <h4>Modal is working</h4>
-        <p>Some text in modal window</p>
-    `,
     width: '400px',
     footerButtons: [{
-            text: 'Ok',
-            type: 'primary',
-            handler() {
-                console.log('Btn primary clicked');
-                modal.close()
-            }
-
+        text: 'Закрыть',
+        type: 'primary',
+        handler() {
+            priceModal.close();
         },
-        {
-            text: 'Cancel',
-            type: 'danger',
-            handler() {
-                console.log('Btn danger clicked');
-                modal.close()
-            }
-        }
-    ]
-})
+    }, ],
+});
+
+document.addEventListener('click', event => {
+    event.preventDefault();
+    const btnType = event.target.dataset.btn;
+    const id = +event.target.dataset.id;
+
+    if (btnType === 'price') {
+        const fruit = fruits.find(f => f.id === id);
+        priceModal.open();
+        console.log(id, fruit);
+    }
+});
