784577f8159725b2b6e4e5a229197526ce151fc5
Iaroslav <kholiavkoi@gmail.com>
2021-10-04 22:52:14 +0300

    a

--- patterns/script.js
+++ patterns/script.js
@@ -1,6 +1,7 @@
 //Structural - Adapter, Decorator, Facade, Proxy, Flyweight
 
-function Mortgage(name) {  //Facade
+//Facade
+function Mortgage(name) {
     this.name = name;
 }
 
@@ -12,33 +13,59 @@ Mortgage.prototype = {
         } else if (!new Credit().get()) {
             result = 'denied';
         } else if (!new Background().check(this.name)) {
-            result = 'denied'
+            result = 'denied';
         }
 
-        return `${this.name} has been ${result} for a ${amount}`
-    }
-}
+        return `${this.name} has been ${result} for a ${amount}`;
+    },
+};
 
 var Bank = function() {
-    this.verify = function (name, amount) {
+    this.verify = function(name, amount) {
         //...logic
         return true;
-    }
-}
+    };
+};
 
 var Credit = function() {
     this.get = function() {
         //...logic
         return true;
-    }
-}
+    };
+};
 
-var Background = function () {
-    this.check = function (name) {
+var Background = function() {
+    this.check = function(name) {
         //...logic
         return true;
-    }
-}
+    };
+};
+
+//Decorator
+var User = function(name) {
+    this.name = name;
+    this.say = function() {
+        console.log(`User: ${this.name}`);
+    };
+};
+
+var DecoratedUser = function(user, street, city) {
+    this.user = user;
+    this.name = user.name;
+    this.street = street;
+    this.city = city;
+    this.say = function() {
+        console.log(`Decorated User: ${this.name}, ${this.city}`);
+    };
+};
+
+var user = new User('Kris');
+user.say();
+
+var decoratedUser = new DecoratedUser(user, 'Broadway', 'New York');
+decoratedUser.say();
+
+
 
 
 //Behavioural
